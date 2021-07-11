f6ac72d524ee21ced6b2f91e49e607c7ead294de
Robert <dev@robertsimoes.org>
2021-07-11 12:49:38 -0700

    Prettier

--- client/src/Web3App.js
+++ client/src/Web3App.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import Navigation from "./components/Navigation"
+import React, { useState, useEffect } from "react";
+import Navigation from "./components/Navigation";
 
 const Web3App = () => {
-	 return (
-			<div className="Web3App">
-				 <Navigation name={"RLP Records"}/>
-				 <p> Record Stacks Go Here </p>
-			</div>
-	 )
-}
+  return (
+    <div className="Web3App">
+      <Navigation name={"RLP Records"} />
+      <p> Record Stacks Go Here </p>
+    </div>
+  );
+};
 
-export default Web3App
+export default Web3App;
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-import { useETHAccounts } from "../hooks/useEthers"
+import { useETHAccounts } from "../hooks/useEthers";
 
 const Navigation = () => {
-	 const acc = useETHAccounts()
+  const acc = useETHAccounts();
 
-	 return (
-			<nav class="">
-				 <div class="container mx-auto">
-						<ul class="navbar-nav">
-							 <li class="nav-item">
-									<a href="#">New</a>
-							 </li>
+  return (
+    <nav class="">
+      <div class="container mx-auto">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a href="#">New</a>
+          </li>
 
-							 <li class="nav-item">
-									<p>{acc}</p>
-							 </li>
-						</ul>
-				 </div>
-			</nav>
-	 )
-}
+          <li class="nav-item">
+            <p>{acc}</p>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Navigation;
