47d15510840eb5cd08965650da1cd99abae5550d
Robert <dev@robertsimoes.org>
2021-07-11 15:04:44 -0700

    Adds Hooks for Repos and Data Calls

---
+++ client/src/hooks/useRepository.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import RecordRepository from "../repos/RecordRepository"
+import RecordLabelRepository from "../repos/RecordLabelRepository"
+
+// Returns the record repository structure
+export const useRecordRepository = () => {
+  const [repo, setRepo] = useState(null);
+
+  useEffect(() => {
+		 const recordRepo = new RecordRepository()
+
+		 setRepo(recordRepo)
+  }, []); // Empty array prevents infinite rerender
+
+  return repo;
+}
+
+// Returns the record label repository structure
+export const useRecordLabelRepository = (labelId) => {
+  const [repo, setRepo] = useState(null);
+
+  useEffect(() => {
+		 const labelRepo = new RecordLabelRepository()
+
+		 setRepo(labelRepo)
+  }, []);
+
+  return repo;
+}
+
