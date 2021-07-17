61d9208de06fd62e60443e69f09069cd3e1281c5
Robert <dev@robertsimoes.org>
2021-07-17 11:56:33 -0700

    Add mock database create methods to record repo

--- client/src/repos/RecordRepository.js
+++ client/src/repos/RecordRepository.js
@@ -1,7 +1,17 @@
-import { REC_HASH_RATE } from "../model/Record";
+import { Record,RECORD_STATES, REC_HASH_RATE } from "../model/Record";
+import { RLP_RECORDS } from "../model/RecordLabel";
 
 // RecordRepository is the data access interface for record
 class RecordRepository {
+	 constructor() {
+			// Setup default records
+			this.records = {
+				 [RLP_RECORDS.id]: [REC_HASH_RATE]
+			}
+
+			console.log("RecordRepository: ", this.records)
+	 }
+
   // Returns all records associated with the label
   listRecords(labelId) {
     const records = this.findRecords(labelId);
@@ -11,9 +21,29 @@ class RecordRepository {
     });
   }
 
+	 createRecord({ labelId, record }) {
+			console.log("RecordRepository: creating new record, label: ", labelId, " record: ", record)
+
+			const rec = Record({
+				 state: RECORD_STATES.DRAFT,
+				 labelId: labelId,
+				 title: record.title,
+				 artist: record.artist
+			})
+
+			// TODO fingerprint
+			 this.records[labelId].push(rec)
+
+    return new Promise((resolve, reject) => {
+
+			 resolve({data: {msg: "done"}});
+    });
+	 }
+
   // FIXME - temporary
-  findRecords(_labelId) {
-    return [REC_HASH_RATE];
+  findRecords(labelId) {
+		 // Use default
+    return this.records[labelId];
   }
 }
 
