a0d4f319d2910f71c0d4a65a832d0ecda0c61f3b
Robert <16901597+aquaflamingo@users.noreply.github.com>
2021-08-13 16:09:19 -0700

    Fix IPFS config URL

--- webclient/src/config.js
+++ webclient/src/config.js
@@ -2,7 +2,7 @@
 // Inject configurations via environment and hydrate
 // in the config object.
 const config = {
-	 IPFS_HTTP_API_URL: process.env.IPFS_HTTP_API_URL || "http://localhost:5002"
+	 IPFS_HTTP_API_URL: process.env.IPFS_HTTP_API_URL || "http://localhost:5001"
 }
 
 export default config;
