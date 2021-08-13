24d5b7c4a7890fe1eaa96ec8350779f0a33eb3aa
Robert <16901597+aquaflamingo@users.noreply.github.com>
2021-08-13 15:19:47 -0700

    Adds content upload to useIPFS

--- webclient/src/hooks/useIPFS.js
+++ webclient/src/hooks/useIPFS.js
@@ -1,7 +1,12 @@
 import { create as CreateIPFSClient } from "ipfs-http-client"
 import config from "../configuration"
 
-const useIPFS = () => {
+const IPFS_ADD_OPTIONS = {
+  cidVersion: 1,
+  hashAlg: 'sha2-256'
+}
+
+export const useIPFS = () => {
 	 const [ipfsClient, setIPFSClient] = useState(null)
 
 	 useEffect(()=>{
@@ -11,3 +16,40 @@ const useIPFS = () => {
 	 return ipfsClient;
 }
 
+export const useIPFSContentUpload = () => {
+	 const ipfs = useIPFS()
+
+	 const upload = useCallback(async (data) => {
+				const ipfsPath = '/nft/' + data.basename
+        const { cid: assetCid } = await ipfs.add({ path: ipfsPath, data.content }, IPFS_ADD_OPTIONS)
+
+        // Create the NFT metadata JSON
+        const assetURI = ensureIPFSPrefix(assetCid) + '/' + basename
+			const metadata = {...data.metadata, uri: assetURI}
+
+        // add the metadata to IPFS
+        const { cid: metadataCid } = await ipfs.add({ path: '/nft/metadata.json', content: JSON.stringify(metadata)}, IPFS_ADD_OPTIONS)
+        const metadataURI = ensureIPFSPrefix(metadataCid) + '/metadata.json'
+
+			return {
+				 metadataURI: metadataURI,
+				 assetURI: assetURI
+			}
+	 })
+
+	 return upload
+}
+
+
+const ensureIPFSPrefix = (cidOrURI) => {
+    let uri = cidOrURI.toString()
+    if (!uri.startsWith('ipfs://')) {
+        uri = 'ipfs://' + cidOrURI
+    }
+
+    if (uri.startsWith('ipfs://ipfs/')) {
+      uri = uri.replace('ipfs://ipfs/', 'ipfs://')
+    }
+
+    return uri
+}
