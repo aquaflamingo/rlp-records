1aa106abb9fbc9570b0294b0668eb179db25d63d
Robert <16901597+aquaflamingo@users.noreply.github.com>
2021-08-15 14:28:57 -0700

    Adds basic useForm

---
+++ webclient/src/hooks/useForm.js
@@ -0,0 +1,55 @@
+import React, { useRef, useState, useEffect } from "react";
+
+const useForm = ({ initialValues, onSubmit }) => {
+  const [values, setValues] = useState(initialValues || {});
+  const [errors, setErrors] = useState({});
+  const [touched, setTouched] = useState({});
+  const [onSubmitting, setOnSubmitting] = useState(false);
+  const [onBlur, setOnBlur] = useState(false);
+
+  const formRendered = useRef(true);
+
+  useEffect(() => {
+    if (formRendered.current) {
+      setValues(initialValues);
+      setErrors({});
+      setTouched({});
+      setOnSubmitting(false);
+      setOnBlur(false);
+    }
+    formRendered.current = false;
+  }, [initialValues]);
+
+  const handleBlur = (event) => {
+    const { target } = event;
+    const { name } = target;
+    setTouched({ ...touched, [name]: true });
+    setErrors({ ...errors });
+  };
+
+  // Set any errors if applicable
+  const handleChange = (event) => {
+    const { target } = event;
+    const { name, value } = target;
+    event.persist();
+    setValues({ ...values, [name]: value });
+  };
+
+  // Set any errors if applicable and submit with onSubmit
+  const handleSubmit = (event) => {
+    if (event) event.preventDefault();
+    setErrors({ ...errors });
+    onSubmit({ values, errors });
+  };
+
+  return {
+    values,
+    errors,
+    touched,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+  };
+};
+
+export default useForm;
