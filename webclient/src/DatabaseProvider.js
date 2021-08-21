07cb78958a14a9f2bfcf27a186eaec0cb98d7cc2
Robert <16901597+aquaflamingo@users.noreply.github.com>
2021-08-21 13:17:52 -0700

    Add mongodb config and DatabaseProvider

---
+++ webclient/src/DatabaseProvider.js
@@ -0,0 +1,10 @@
+import db from "./db"
+
+const DatabaseProvider = ({ children }) => {
+	 // How to connect to DB?
+  return (
+		 <div>{children}</div>
+  );
+};
+
+export default DatabaseProvider;
--- webclient/src/config.js
+++ webclient/src/config.js
@@ -3,6 +3,7 @@
 // in the config object.
 const config = {
   IPFS_HTTP_API_URL: process.env.IPFS_HTTP_API_URL || "http://localhost:5001",
+	 MONGO_DB_URI: process.env.MONGO_DB_URI || "mongodb://localhost:27017/rlp",
 };
 
 export default config;
