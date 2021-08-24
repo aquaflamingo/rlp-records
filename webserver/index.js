7e822b49b103663d694a09671c9e6e0449e6a906
Robert <16901597+aquaflamingo@users.noreply.github.com>
2021-08-24 08:32:02 -0700

    Adds basic webserver

--- webserver/index.js
+++ webserver/index.js
@@ -1,9 +1,10 @@
 import express from "express"
 import db from "./db.js"
 
-// const express = require('express');
 const app = express();
 const port = 3000
 
-app.get('/',(req,res)=>res.send("status: OK"))
+app.get('/',(req,res)=>{
+	 res.send("status: OK")
+})
 app.listen(port, () => console.log(`Express server running at http:\/\/127.0.0.1:${port}`));
--- webserver/models/Member.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const MemberSchema = new mongoose.Schema({
-  name: String,
-});
-
-const Member = mongoose.model("Member", MemberSchema);
-
-export default Member;
