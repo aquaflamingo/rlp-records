87c48d6463f144b2c950253ed7deae04a011df34
Robert <16901597+aquaflamingo@users.noreply.github.com>
2021-08-27 08:37:50 -0700

    Init django app destroy express

--- webserver/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-const router = express.Router();
-
-/* GET home page. */
-router.get('/', (req, res, next) => {
-	 res.send("status: OK")
-});
-
-export default router;
