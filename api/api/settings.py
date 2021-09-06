90e25c3db2338735fd73e2a1bcdec76d39d17751
Robert <16901597+aquaflamingo@users.noreply.github.com>
2021-09-06 11:47:25 -0700

    Adds CORS headers to Django app

--- api/api/settings.py
+++ api/api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'rest_framework',
     'django_filters',
     'rlp_records.apps.RlpRecordsConfig',
@@ -159,3 +160,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
