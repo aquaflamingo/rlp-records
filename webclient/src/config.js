50171fa8c5becf6c4eb3fb9afa2bb258b7af1287
Robert <16901597+aquaflamingo@users.noreply.github.com>
2021-09-08 08:44:49 -0700

    Updates configuration URI with API URI

--- webclient/src/config.js
+++ webclient/src/config.js
@@ -3,6 +3,7 @@
 // in the config object.
 const config = {
   IPFS_HTTP_API_URL: process.env.IPFS_HTTP_API_URL || "http://localhost:5001",
+  API_URL: process.env.API_URL || "http://localhost:8000",
 };
 
 export default config;
