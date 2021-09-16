19f0f255ad4bd533d9e6ca55f790f31616ee8373
Robert <16901597+aquaflamingo@users.noreply.github.com>
2021-09-16 08:29:34 -0700

    Renames AudioFile.value -> file and change type to FileField

--- api/rlp_records/models.py
+++ api/rlp_records/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify 
 
 class ERC721(models.Model):
     tokenId = models.CharField(max_length=200)
@@ -26,7 +27,11 @@ class Record(models.Model):
 
 
 class AudioFile(models.Model):
+    def user_upload_path(self, instance, filename):
+        # file saved to MEDIA/uploads/user_id/file_name
+        return 'uploads/user_{0}/{1}'.format(instance.user.id, slugify(filename))
+
     sha256 = models.CharField(max_length=200, null=True)
     fingerprint = models.BinaryField(null=True)
-    value = models.BinaryField(null=True)
+    file = models.FileField(upload_to=user_upload_path, null=True)
     record = models.ForeignKey(Record, on_delete=models.CASCADE, null=True)
