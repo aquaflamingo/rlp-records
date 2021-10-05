61491c137485b62c93c85db5313f426d5a3fce9f
Robert <16901597+aquaflamingo@users.noreply.github.com>
2021-10-05 08:41:59 -0700

    Adds useCreateMintEvent hook

---
+++ webclient/src/hooks/useCreateMintEvent.js
@@ -0,0 +1,30 @@
+import React, { useCallback, useState, useEffect } from "react";
+import { useERC721Repository } from "../hooks/useRepository";
+
+export const useCreateMintEvent = () => {
+  const [result, setResult] = useState({
+    data: null,
+    error: null,
+    isLoading: false,
+  });
+
+  const repo = useERC721Repository();
+
+  const request = ({ proof, recordId, tokenId, assetURI, metadataURI, storageVenue}) => {
+    setResult((prev) => ({ ...prev, isLoading: true }));
+
+    repo
+      .createMintEvent({ proof, recordId, tokenId, metadataURI})
+      .then((res) => {
+				 debugger
+        // FIXME: error if data is not array
+        setResult({ data: res.data, isLoading: false, error: null });
+      })
+      .catch((error) => {
+        console.error("Failed to mint record:", error);
+        setResult({ data: [], isLoading: false, error });
+      });
+  };
+
+  return [result, request];
+}
