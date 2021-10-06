3e496763c8a93a331ef455e6eaf6342183d87b57
Robert <16901597+aquaflamingo@users.noreply.github.com>
2021-10-06 08:33:36 -0700

    Additional edits on EventRepository

--- webclient/src/repos/EventRepository.js
+++ webclient/src/repos/EventRepository.js
@@ -1,31 +1,32 @@
 import client from "./ApiClient";
 import Base from "./Base";
-// TODO: create event deserializer
 import { EventsDeserializer } from "./models/Event.js";
 
 // EventRepository is the data access interface  for labels
 class EventRepository extends Base {
-  constructor() {
-    super();
-    this.URI = "/events/";
-  }
+	 constructor() {
+			super();
+			this.URI = "/events/";
+	 }
 
+	 // Request to create an event on the backend
+	 // using the polymorphic event endpoint
 	 async createMintEvent({ proof, details}) {
-    try {
-      let response = await client.post(`/events/`, {
-				 event_type: 'MINT',
-				 proof: proof,
-				 details
-			})
+			try {
+				 let response = await client.post(`/events/`, {
+						event_type: 'MINT',
+						proof,
+						details
+				 })
 
-      const res = response.data;
+				 const res = response.data;
 
-      return res;
-    } catch (err) {
-      console.error(err);
-      debugger;
-      return null;
-    }
+				 return res;
+			} catch (err) {
+				 console.error(err);
+				 debugger;
+				 return null;
+			}
 	 }
 }
 
