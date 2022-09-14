9ac21b8c314f3c93e082eb551c0a1e1ac6ffcde4
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-09-14 16:39:38 -0400

    client: Adds list method to RecordLabelRepository and deserializer

--- webclient/src/repos/RecordLabelRepository.js
+++ webclient/src/repos/RecordLabelRepository.js
@@ -1,5 +1,6 @@
 import client from "./ApiClient";
 import Base from "./Base";
+import { RecordLabelDeserializer } from "../models/RecordLabel";
 
 // RecordLabelRepository is the data access interface  for labels
 class RecordLabelRepository extends Base {
@@ -8,6 +9,30 @@ class RecordLabelRepository extends Base {
     this.URI = "/recordlabels/";
   }
 
+  // Fetches a list of all available record labelsa
+  async list() {
+    try {
+      const response = await client.get(
+        `${this.URI}`
+      );
+
+      console.log("RecordLabelRepository.list:", response);
+
+      let result = [];
+
+      if (response && response.data) {
+        result = this.deserializeResponse(
+          response.data,
+          RecordLabelDeserializer
+        );
+      }
+
+      return result;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
   // Returns all records associated with the label
   async get(id) {
     if (id == undefined) {
