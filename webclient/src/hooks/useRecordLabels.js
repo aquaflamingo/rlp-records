6b73260ea844b27f326ad9a8a464876a0de124c5
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-09-14 16:41:10 -0400

    client: Add useRecordLabels hook

---
+++ webclient/src/hooks/useRecordLabels.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import {useRecordLabelRepository} from "./useRepository"
+
+export const useRecordLabel = () => {
+  const [label, setlabel] = useState(null);
+
+  useEffect(() => {
+    setlabel();
+  });
+
+  return label;
+};
+export const useRecordLabels = () => {
+  // FIXME - reach out to chain to get record label membership
+  const [labels, setLabels] = useState(null);
+
+  const repo = useRecordLabelRepository();
+
+  useEffect(() => {
+    repo
+      ?.list()
+      .then((recLabels) => {
+        if (recLabels) {
+          console.log("Fetched ", recLabels.length, " labels");
+          setLabels(recLabels);
+        }
+      })
+      .catch((err) => {
+        console.error("Failed to fetch record labels", err);
+        debugger;
+      });
+  }, [repo]);
+
+  return labels;
+};
