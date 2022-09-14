9a7685416d84b3fe2533b64b48e2908487c55d57
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-09-14 14:50:52 -0400

    client: Adds getFromWallet functionality to MemberRepository
    * Need to test, given VPN problems

--- webclient/src/repos/MemberRepository.js
+++ webclient/src/repos/MemberRepository.js
@@ -8,6 +8,23 @@ class MemberRepository extends Base {
     super();
     this.URI = "/members/";
   }
+  async getFromWallet({ walletAddress }) {
+    try {
+      const response = await client.get(`${this.URI}?wallet_address=${labelId}`);
+      console.log("MemberRepository.list:", response);
+
+      var result;
+
+      if (response && response.data) {
+        result = this.deserializeResponse(response.data, MembersDeserializer);
+      }
+
+      return result;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
 
   async list({ labelId }) {
     try {
--- webclient/src/repos/models/Member.js
+++ webclient/src/repos/models/Member.js
@@ -2,5 +2,6 @@ export const MembersDeserializer = (data) => {
   return {
     id: data.id,
     name: data.name,
+    wallet_address: data.wallet_address,
   };
 };
